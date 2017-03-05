1969e4aa885fae0ad930512a62b34f91cfdbaf57
sszqdz <boboohappy@163.com>
2017-03-05 22:23:56 +0800

    add three-tier about backend

--- scripts/backend/blog_content_fetch.js
+++ scripts/backend/blog_content_fetch.js
@@ -1,34 +1,37 @@
 /**
+ * 获取文章数据接口
  * Created by hasee on 2017/2/28.
  */
+
 const Resources = require('../utils/Resources.js');
 const FS = require('fs');
 
 var getBlogContentByFile = function (blogId) {
-    var dataObject = {
-        success: true,
+    var dataObj = {
+        // success: true,
         data: []
     };
-    //其实这里应该按照blogId来读取博客内容，有待商榷
-    FS.readFile(Resources.getJsonFile('blog_content_search.json'), 'utf-8', function (err, data) {
+    //这里修改为根据blogId选择对应的JSON文件，但是要不要做一个文件的桥，还需商榷
+    var blogName = blogId + '.json';
+    FS.readFile(Resources.getJsonFile(blogName), 'utf-8', function (err, data) {
         if (err) {
-            dataObject.success = false;
+            dataObj = false;
         } else {
-            dataObject.data = JSON.parse(data);
+            dataObj.data = JSON.parse(data);
         }
     });
-    return dataObject;
-}
+    return dataObj;
+};
 
 var getBlogContentByDB = function (blogId) {
     var dataObject = {
-        success: true,
+        // success: true,
         data: []
     };
 
     return dataObject;
-}
+};
 
 module.exports = function (blogId) {
     return getBlogContentByFile(blogId);
-}
+};
--- scripts/service/blog_content_search.js
+++ scripts/service/blog_content_search.js
@@ -1,20 +1,20 @@
-const getBlogContent = require('./blog_content_middleware');
+const BackendEngine = require('./../backend/BackendEngine.js');
 
 module.exports = function (req, res) {
     var blogId = req.body.id;
     var response = {
-        result: 0,
+        result: 1,
         data: []
     };
-    if (blogId) {
-        var dataObject = getBlogContent(blogId);
 
-        if (dataObject.success) {
+    if (blogId) {
+        var dataObject = BackendEngine.Invoke('blog_content_fetch', blogId);
+        if (dataObject) {
             response.data = dataObject.data;
-        } else {
-            response.result = 1;
+            response.result = 0;
         }
     }
+
     res.headers("Content-type", "application/json");
     res.send(response);
     res.end();
